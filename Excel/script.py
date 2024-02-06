4e6215785baf37fdb89f77ce5babdc9bcd33989b
Cauê <cauusaantos@gmail.com>
2024-02-05 21:57:43 -0300

    feat

--- Excel/script.py
+++ Excel/script.py
@@ -2,9 +2,9 @@ import pandas as pd   # Max Linhas: 7
 from IPython.display import display
 
 valores_df = pd.read_excel(r'C:\Users\CAUÊ\Documents\Pandas\Excel\tst.xlsx')
-#valores_df.loc[:, 'Zero'] = 300 / 3
-valores_df = valores_df.drop('Quantidade', axis=1)
+
+valores_dez = pd.read_excel(r'C:\Users\CAUÊ\Documents\Pandas\Excel\valores-dez.xlsx')
+
+valores_df = valores_df.merge(valores_dez)
+
 display(valores_df)
- 
-
-
---
+++ Script/first_script.py
@@ -0,0 +1,7 @@
+from IPython.display import display
+import pandas as pd
+
+planilha = pd.read_excel(r'c:\Users\CAUÊ\Documents\Pandas\Script\tst.xlsx')
+planilha.loc[planilha["Cor"] == "Azul", "Quantidade"] = 1 # Modificando Células na tabela
+
+display(planilha)
