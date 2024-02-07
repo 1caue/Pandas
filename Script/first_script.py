ae1ec76aec618b07b5cdeb62e1093c8f4b52d6d2
Cauê <cauusaantos@gmail.com>
2024-02-06 21:49:24 -0300

    test

--- Script/first_script.py
+++ Script/first_script.py
@@ -5,3 +5,4 @@ planilha = pd.read_excel(r'c:\Users\CAUÊ\Documents\Pandas\Script\tst.xlsx')
 planilha.loc[planilha["Cor"] == "Azul", "Quantidade"] = 1 # Modificando Células na tabela
 
 display(planilha)
+
