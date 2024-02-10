d7abe4a761c7726d7a556ae12937bf456e73c2fe
Cauê <cauusaantos@gmail.com>
2024-02-09 21:59:01 -0300

    test

--- Excel/script.py
+++ Excel/script.py
@@ -5,6 +5,7 @@ valores_df = pd.read_excel(r'C:\Users\CAUÊ\Documents\Pandas\Excel\tst.xlsx')
 
 valores_dez = pd.read_excel(r'C:\Users\CAUÊ\Documents\Pandas\Excel\valores-dez.xlsx')
 
-valores_df = valores_df.merge(valores_dez)
+valores_dez['Colunas1'] = valores_dez['Colunas2'].apply(lambda x: x * 3)
 
-display(valores_df)
+valores_dez.to_excel('Nova_Tabela.xlsx', index=False)
+display(valores_dez)
