67b6c5085da586ae5e66da12f5d73b49799d6c5d
Cauê <cauusaantos@gmail.com>
2024-02-10 20:55:54 -0300

    fix

--- Excel/script.py
+++ Excel/script.py
@@ -1,11 +1,14 @@
 import pandas as pd   # Max Linhas: 7
 from IPython.display import display
+# valores_dez['Colunas1'] = valores_dez['Colunas2'].apply(lambda x: x * 3)
+# valores_dez.to_excel('Nova_Tabela.xlsx', index=False)
 
 valores_df = pd.read_excel(r'C:\Users\CAUÊ\Documents\Pandas\Excel\tst.xlsx')
 
 valores_dez = pd.read_excel(r'C:\Users\CAUÊ\Documents\Pandas\Excel\valores-dez.xlsx')
 
-valores_dez['Colunas1'] = valores_dez['Colunas2'].apply(lambda x: x * 3)
+colum = input('Quer analisar qual coluna?: ')
 
-valores_dez.to_excel('Nova_Tabela.xlsx', index=False)
-display(valores_dez)
+analise = valores_dez.sort_values(by=f'{colum}', ascending=False)
+
+print(analise)
